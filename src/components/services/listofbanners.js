9c8abaa5e54906dce3e8b5f472c03113cd25d11e
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-07-14 18:32:03 -0500

    🔨 add banners section

---
+++ src/components/services/listofbanners.js
@@ -0,0 +1,45 @@
+
+
+export const banners = [
+    {
+        title: "deep earth",
+        imageDesktop: "/src/assets/images/desktop/image-deep-earth.jpg",
+        imageMobile: "/src/assets/images/mobile/image-deep-earth.jpg"
+    },
+    {
+        title: "night arcade",
+        imageDesktop: "/src/assets/images/desktop/image-night-arcade.jpg",
+        imageMobile: "/src/assets/images/mobile/image-night-arcade.jpg"
+    },
+    {
+        title: "soccer team vr",
+        imageDesktop: "/src/assets/images/desktop/image-soccer-team.jpg",
+        imageMobile: "/src/assets/images/mobile/image-soccer-team.jpg"
+    },
+    {
+        title: "the grid",
+        imageDesktop: "/src/assets/images/desktop/image-grid.jpg",
+        imageMobile: "/src/assets/images/mobile/image-grid.jpg"
+    },
+    {
+        title: "from up above vr",
+        imageDesktop: "/src/assets/images/desktop/image-from-above.jpg",
+        imageMobile: "/src/assets/images/mobile/image-from-above.jpg"
+    },
+    {
+        title: "pocket borealis",
+        imageDesktop: "/src/assets/images/desktop/image-pocket-borealis.jpg",
+        imageMobile: "/src/assets/images/mobile/image-pocket-borealis.jpg"
+    },
+    {
+        title: "the curiosity",
+        imageDesktop: "/src/assets/images/desktop/image-curiosity.jpg",
+        imageMobile: "/src/assets/images/mobile/image-curiosity.jpg"
+    },
+    {
+        title: "make it fisheye",
+        imageDesktop: "/src/assets/images/desktop/image-fisheye.jpg",
+        imageMobile: "/src/assets/images/mobile/image-fisheye.jpg"
+    },
+]
+
