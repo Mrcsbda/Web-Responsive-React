0c8e2acb3401fc3fd4e8d809e46092d6dc2fce11
José Henao <josexdjose14@gmail.com>
2023-07-14 11:09:21 -0500

    mobile layout finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import "./footer.scss"
+
+const list = ["About", "Careers", "Events", "Products", "Supports"]
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <section>
+      <article className="leftSide">
+        <h3>loopstudios</h3>
+        <ul>
+          {
+            list.map((item, index) => (<li key={index + 1}>{item}</li>))
+
+          }
+        </ul>
+
+        {/* <ul>
+          <li>About</li>
+          <li>Careers</li>
+          <li>Events</li>
+          <li>Products</li>
+          <li>Support</li>
+        </ul> */}
+
+      </article>
+      <article className="rightSide">
+        <figure>
+          <img src="src/assets/images/icon-facebook.svg" alt="icon-facebook" />
+          <img src="src/assets/images/icon-twitter.svg" alt="icon-twitter" />
+          <img src="src/assets/images/icon-pinterest.svg" alt="icon-pinterest" />
+          <img src="src/assets/images/icon-instagram.svg" alt="icon-instagram" />
+        </figure>
+        <p>@ 2021 Loopstudios. All rights reserved.</p>
+      </article>
+    </section>
+
   )
 }
 
